b2f7c9629a3d449ab03dfed3ce28457bd20220af
Cameron Brown <cameron.scott.brown@gmail.com>
2019-02-22 10:27:22 -0800

    added logo and started form

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+  return (
+    <div className='input-form'>
+      HELLO
+    </div>
+  );
+}
+
+export default ImageLinkForm;
