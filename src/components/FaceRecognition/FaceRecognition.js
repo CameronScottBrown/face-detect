d7ed1d0c86f8afbebe619942af3bf863239cb1b5
Cameron Brown <cameron.scott.brown@gmail.com>
2019-02-25 15:31:38 -0800

    unsure

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./FaceRecognition.css";
 
-const FaceRecognition = () => {
-  return (
-    <div>I recognize this face</div>
-  );
+const FaceRecognition = ({ imageUrl }) => {
+
+  if (imageUrl){
+    return (
+      <div className='face-box'>
+        <img alt='face detection result' src={imageUrl}/>
+      </div>
+    );
+  } else {
+    return (
+      <div></div>
+    )
+  }
+  
 };
 
 export default FaceRecognition;
